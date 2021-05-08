0153df64de771eaa91e3c304813616d00836aac4
Desarrollo <Desarrollo@DESKTOP-61GMHDM>
2021-05-07 20:51:20 -0400

    Version 1

---
+++ src/main/java/challenge/backend/disney/ChallengeJavaDisneyApplication.java
@@ -0,0 +1,13 @@
+package challenge.backend.disney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeJavaDisneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeJavaDisneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/challenge/backend/disney/ChallengeJavaDisneyApplicationTests.java
@@ -0,0 +1,13 @@
+package challenge.backend.disney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeJavaDisneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
